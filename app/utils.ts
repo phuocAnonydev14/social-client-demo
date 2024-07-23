d725d5eb9597ce67781afb23568df42d62c4c5f7
phuoc <phuoc.anonydev2k3@gmail.com>
2024-07-23 17:17:23 +0700

    f

--- app/utils.ts
+++ app/utils.ts
@@ -1,6 +1,6 @@
 import { SocialLogin } from "aa-conla-social-sdk";
 
 export const social = new SocialLogin(
-  "http://localhost:3000/oauth",
-  "http://localhost:3000"
+  "https://social-client-demo.vercel.app/oauth",
+  "https://social-client-demo.vercel.app"
 );
